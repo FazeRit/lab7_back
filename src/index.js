bf0b460018e5000f9d997d2760929a61c301239a
denis <denisgutzan.com>
2024-12-09 15:32:43 +0200

    peremoga rostov2.0 siria

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import cors from 'cors';
 const app = express();  
 app.use(express.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:5500',
+    origin: 'https://lab6-backend.vercel.app/',
     methods: 'GET,POST,PUT,DELETE',
 }));
 app.use('/api', router);    
